39839992cc3000d3a507102e081e2c8df5bc4cad
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2026-04-15 01:12:25 +0300

    Add Terraform lesson-5 without terraform cache

---
+++ lesson-5/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "serhii-terraform-state-lesson-5"
+    key            = "lesson-5/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
---
+++ lesson-5/modules/s3-backend/s3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
